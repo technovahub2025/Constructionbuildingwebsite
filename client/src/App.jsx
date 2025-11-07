3b2f85e86002a0739370353410987c65558138d3
Technovahub Career <technovahubcareer@gmail.com>
2025-11-07 14:30:44 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Pricing from "./pages/Pricing";
 
 const App = () => {
   return (
-    <div className=" bg-white">
+    <div className=" bg-white  md:px-10 ">
       <BrowserRouter>
       
       
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import Slider from '../components/Hero'
 import Services from '../components/Services'
 import OurServices from '../components/OurServices'
 
 const Home = () => {
   return (
-    <div> <main>
-        <Hero/>
+    <div className='mt-9'> <main>
+        <Slider/>
           <Services />
           <OurServices/>
       </main></div>
